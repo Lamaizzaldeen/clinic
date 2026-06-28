07f50ab684283d8b8fe86620857adcb337693144
Lamaizzaldeen <lamaizzaldeen@gmail.com>
2026-06-29 00:40:19 +0300

    add include path for subscriptions

--- src/urls.py
+++ src/urls.py
@@ -53,8 +53,9 @@ urlpatterns = [
     # Contact
     path('api/contact/', include('contact.urls')),
 
-
-
+    #subscriptions
+    path('api/workshops/', include('subscriptions.urls')),
+    
     # Optional UI:
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
